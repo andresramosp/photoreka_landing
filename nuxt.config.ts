d6c5c42478489a442ab9ae4e643f0812958d3d31
anramosp <anramosp.ext@acciona.com>
2026-06-15 15:45:36 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,8 @@ export default defineNuxtConfig({
   modules: ["nuxtjs-naive-ui", "@nuxtjs/color-mode"],
   css: ["~/assets/global.scss"],
 
-  // Configuración SSR para SEO óptimo
-  ssr: true,
+  // Static generation mode to reduce runtime memory usage
+  ssr: false,
 
   runtimeConfig: {
     public: {
